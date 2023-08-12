3d8ea05761fab1f0ece264e497642dbe79455254
JonesRod <batatajonesrodrigues@gmail.com>
2023-08-12 10:08:26 -0400

    criada lista de estados

---
+++ php/admin_config.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Configuração</title>
+</head>
+<body>
+    <h1>Configuração</h1>
+    <form action="">
+        <ul>
+            <li>E-mail denotificação:</li>
+            <li>E-mail de recuperação de senha:</li>
+
+        </ul>
+        <p>
+            <a href="">Voltar</a>
+            <button type="submit">Salvar</button>
+        </p>
+    </form>
+</body>
+</html>
--- php/ficha_inscricao.php
+++ php/ficha_inscricao.php
@@ -8,7 +8,7 @@
     if(!isset($_SESSION['usuario'])){
         header("Location: ../login.php");
     }*/
-    $caminhoDaImagem= false;
+    //$caminhoDaImagem= false;
 ?>
 <!DOCTYPE html>
 <html lang="pt">
@@ -19,18 +19,19 @@
 </head>
 <body>
     <h1>Ficha de Inscrição</h1>
-    <form method="POST" action="">
+    <form action="processar_upload.php" method="POST" enctype="multipart/form-data">
         <div>
-            <img src="<?php echo $caminhoDaImagem; ?>" alt="Sem Foto"><br>
-            <a href="">Adicione uma foto de perfil.</a>
+            <img src="../arquivos/9734564-default-avatar-profile-icon-of-social-media-user-vetor.jpg" alt=""><br>
+            <input type="file" name="imagem" id="imagem"><br>
+            <input type="submit" value="Adicionar">  
         </div>    
         <p>
             <label>Nome: </label>
-            <input value="<?php if(isset($_POST['nome'])) echo $_POST['nome']; ?>" name="nome" type="text"><br>
+            <input required value="<?php if(isset($_POST['nome'])) echo $_POST['nome']; ?>" name="nome" type="text"><br>
         </p>
         <p>
             <label>Sobrenome: </label>
-            <input value="<?php if(isset($_POST['sobrenome'])) echo $_POST['sobrenome']; ?>" name="sobrenome" type="text"><br>
+            <input required value="<?php if(isset($_POST['sobrenome'])) echo $_POST['sobrenome']; ?>" name="sobrenome" type="text"><br>
         </p>
         <p>
             <label>Apelido: </label>
@@ -38,27 +39,44 @@
         </p>
         <p>
             <label>CPF: </label>
-            <input value="<?php if(isset($_POST['cpf'])) echo $_POST['cpf']; ?>" name="cpf" type="text"><br>
+            <input required value="<?php if(isset($_POST['cpf'])) echo $_POST['cpf']; ?>" name="cpf" type="number"><br>
         </p>
         <p>
             <label>RG: </label>
-            <input value="<?php if(isset($_POST['rg'])) echo $_POST['rg']; ?>" name="rg" type="text"><br>
+            <input required value="<?php if(isset($_POST['rg'])) echo $_POST['rg']; ?>" name="rg" type="number"><br>
         </p>
         <p>
             <label>Data de Nascimento: </label>
-            <input value="<?php if(isset($_POST['nascimento'])) echo $_POST['nascimento']; ?>" name="nascimento" type="text"><br>
+            <input required value="<?php if(isset($_POST['nascimento'])) echo $_POST['nascimento']; ?>" name="nascimento" type="date"><br>
+        </p>
+        <p> 
+            <!--<input required value="<?php if(isset($_POST['uf'])) echo $_POST['uf']; ?>" name="uf" type="text"><br>-->
+            
+            <label>Estado: </label><select name="uf" id="uf" value="<?php if(isset($_POST['uf'])) echo $_POST['uf']; ?>">
+            <option value="AC">Acre</option>
+            <option value="AL">Alagoas</option>
+            <option value="AP">Amapá</option>
+            <option value="AM">Amazonas</option>
+            <!-- Adicione mais opções para outros estados aqui -->
+            </select>
         </p>
         <p>
             <label>Cidade Natal: </label>
-            <input value="<?php if(isset($_POST['cidnatal'])) echo $_POST['cidnatal']; ?>" name="cidnatal" type="text"><br>
+            <input required value="<?php if(isset($_POST['cidnatal'])) echo $_POST['cidnatal']; ?>" name="cidnatal" type="text"><br>
+        </p>
+
+        <p>
+            <label>Nome da Mãe: </label>
+            <input required value="<?php if(isset($_POST['mae'])) echo $_POST['mae']; ?>" name="mae" type="data"><br>
         </p>
         <p>
-            <label>UF: </label>
-            <input value="<?php if(isset($_POST['uf'])) echo $_POST['uf']; ?>" name="uf" type="text"><br>
+            <label>Nome do Pai: </label>
+            <input value="<?php if(isset($_POST['pai'])) echo $_POST['pai']; ?>" name="pai" type="text"><br>
         </p>
+
         <p>
             <label>Celular 1: </label>
-            <input value="<?php if(isset($_POST['celular1'])) echo $_POST['celular1']; ?>" name="celular1" type="text"><br>
+            <input required value="<?php if(isset($_POST['celular1'])) echo $_POST['celular1']; ?>" name="celular1" type="text"><br>
         </p>
         <p>
             <label>Celular 2: </label>
@@ -66,25 +84,26 @@
         </p>
         <p>
             <label>Endereço: </label>
-            <input value="<?php if(isset($_POST['endereco'])) echo $_POST['endereco']; ?>" name="endereco" type="text"><br>
+            <input required value="<?php if(isset($_POST['endereco'])) echo $_POST['endereco']; ?>" name="endereco" type="text"><br>
         </p>
         <p>
             <label>N°: </label>
-            <input value="<?php if(isset($_POST['numero'])) echo $_POST['numero']; ?>" name="numero" type="text"><br>
+            <input required value="<?php if(isset($_POST['numero'])) echo $_POST['numero']; ?>" name="numero" type="text"><br>
         </p>
         <p>
             <label>Bairro: </label>
-            <input value="<?php if(isset($_POST['bairro'])) echo $_POST['bairro']; ?>" name="bairro" type="text"><br>
+            <input required value="<?php if(isset($_POST['bairro'])) echo $_POST['bairro']; ?>" name="bairro" type="text"><br>
         </p>
         <p>
             <label>E-mail:</label>
-            <input value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" name="email" type="email"><br>
+            <input required value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" name="email" type="email"><br>
         </p>
         <p>
             <label for="">Diga qual é o motivo ao qual você deseja se tornar sócio: </label><br>
-            <input placeholder="Minimo 100 digitos" value="<?php if(isset($_POST['motivo'])) echo $_POST['motivo']; ?>" type="text" name="motivo">
+            <textarea required placeholder="Minimo 100 digitos" rows="4" cols="50" value="<?php if(isset($_POST['motivo'])) echo $_POST['motivo']; ?>" type="text" name="motivo"></textarea>
         </p>
         <p>
+        <input type="checkbox" name="aceito" value="sim">Eu aceito os <a href="">Termos.</a><br>
             <a href="../index.php">Voltar</a>
             <button type="submit">Salvar</button>
         </p>
